748de9990025e5bf098a7dc30cdeb4811612439e
liucaijing <2729036362@qq.com>
2022-03-23 08:29:43 +0800

    初始化项目

---
+++ src/main/java/com/xlhj/jenkins/GithubJenkinsApplication.java
@@ -0,0 +1,17 @@
+package com.xlhj.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: liucaijing
+ * @description: GithubJenkinsApplication
+ * @date: 2022/3/23 8:25
+ */
+@SpringBootApplication
+public class GithubJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubJenkinsApplication.class, args);
+    }
+}
